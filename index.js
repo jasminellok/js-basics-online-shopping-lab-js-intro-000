84003e3fc459b1d9e389d8f2b5588cc096898b9b
Jasmine Lok <jasminellok@gmail.com>
2020-04-13 06:40:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ function placeOrder(cardNumber) {
   if (cardNumber >0) {
     var cart = []
     return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
   }
 
 }
